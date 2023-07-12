5bc2d08d67db4e093bf18e31df680cf71e362147
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-12 23:04:00 +0200

    Push

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -10,7 +10,12 @@ const BillboardPage = async ({
       id: params.billboardId,
     },
   });
-  return <div>Existing Billboard: {billboard?.label}</div>;
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8  pt-6"></div>
+    </div>
+  );
 };
 
 export default BillboardPage;
