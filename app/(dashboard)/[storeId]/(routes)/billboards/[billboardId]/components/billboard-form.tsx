8cd8d1ecb2947c083bf696caea4dcddc1694a172
Krishan Ramdin <Krishanramdin_@hotmail.com>
2023-07-21 14:34:59 +0200

    Push Changes Upstream

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/components/billboard-form.tsx
@@ -86,7 +86,7 @@ export const BillboardForm: React.FC<BillboardFormProps> = ({
         `/api/${params.storeId}/billboards/${params.billboardId}`
       );
       router.refresh();
-      router.push("/");
+      router.push(`/${params.storeId}/billboards`);
       toast.success("Billboard succesfully deleted!");
     } catch (error) {
       toast.error(
